3096c16c60500ae323a99ef275de2af5e8dd9a25
Aniket Katkar <aniketkatkar97@gmail.com>
2022-12-02 13:18:53 +0530

    Optimized code for note component

--- components/common/Note/Note.tsx
+++ components/common/Note/Note.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 
-import CalloutStyles from "./callout.module.css";
-import NoteStyles from "./Note.module.css";
+import styles from "./Note.module.css";
 
 import { ReactComponent as Pin } from "../../../images/icons/pin.svg";
 
 const Note = ({ children }) => {
   return (
-    <section className={`${CalloutStyles.Container} ${NoteStyles.Note}`}>
+    <section className={styles.Container}>
       <span>
-        <Pin className={NoteStyles.Icon} />
+        <Pin className={styles.Icon} />
       </span>
-      {children}
+      <div>{children}</div>
     </section>
   );
 };
