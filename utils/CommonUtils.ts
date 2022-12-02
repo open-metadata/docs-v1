6087dfad598036ad740eaad1271e148f90650ee5
Aniket Katkar <aniketkatkar97@gmail.com>
2022-12-02 19:12:58 +0530

    Added util function for getting heading id from heading

--- utils/CommonUtils.ts
+++ utils/CommonUtils.ts
@@ -1,3 +1,7 @@
 export const getDivIndexFromId = (id: string) => {
   return Number(id.split("-").reverse()[0]);
 };
+
+export const generateIdFromHeading = (heading: string) => {
+  return heading.toLowerCase().replaceAll(" ", "-");
+};
