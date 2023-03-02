7e6ed12a6f0e2806c1e323ea892efdb305891a6b
Aniket Katkar <aniketkatkar97@gmail.com>
2023-03-02 11:09:39 +0530

    Fixed bug for links where external links were also being modified with versions

--- components/common/CustomAnchorNode/CustomAnchorNode.tsx
+++ components/common/CustomAnchorNode/CustomAnchorNode.tsx
@@ -4,10 +4,13 @@ import { getUrlWithVersion } from "../../../utils/CommonUtils";
 interface Props {
   href: string;
   children: ReactNode;
-  isExternalLink: boolean;
 }
 
-function CustomAnchorNode({ href, children, isExternalLink = false }: Props) {
+function CustomAnchorNode({ href, children }: Props) {
+  const regexToIdentifyLink = /^(http|https|ftp|www)/g;
+
+  const isExternalLink = href.search(regexToIdentifyLink) !== -1;
+
   return (
     <a href={isExternalLink ? href : getUrlWithVersion(href)}>{children}</a>
   );
