17f85c281ca799ce4d6d512f287b3505de6a8d4c
Aniket Katkar <aniketkatkar97@gmail.com>
2023-03-06 19:31:04 +0530

    changes to use new SelectDropdown component for version selector

--- constants/version.constants.ts
+++ constants/version.constants.ts
@@ -1,1 +1,10 @@
+import { SelectOption } from "../components/SelectDropdown/SelectDropdown";
+
 export const DEFAULT_VERSION = "v0.13.2";
+
+export const VERSION_SELECT_DEFAULT_OPTIONS: Array<SelectOption<string>> = [
+  {
+    label: DEFAULT_VERSION,
+    value: DEFAULT_VERSION,
+  },
+];
