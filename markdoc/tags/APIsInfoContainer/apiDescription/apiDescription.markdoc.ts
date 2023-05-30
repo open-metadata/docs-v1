5ad59649b3f7cdfd12d68795c13eebbd423bb453
Aniket Katkar <aniketkatkar97@gmail.com>
2023-05-30 19:49:04 +0530

    Updated README file content

--- markdoc/tags/APIsInfoContainer/apiDescription/apiDescription.markdoc.ts
+++ markdoc/tags/APIsInfoContainer/apiDescription/apiDescription.markdoc.ts
@@ -1,9 +1,5 @@
 export const apiDescription = {
   render: "APIDescription",
   description: "Description about the API.",
-  attributes: {
-    title: {
-      type: String,
-    },
-  },
+  attributes: {},
 };
