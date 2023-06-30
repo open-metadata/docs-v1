d84a4f00b42430ea10cd80ec6995b0dc1ea0998f
Aniket Katkar <aniketkatkar97@gmail.com>
2023-06-30 16:25:59 +0530

    changed the default version for version selector

--- constants/version.constants.ts
+++ constants/version.constants.ts
@@ -1,7 +1,7 @@
 import { SelectOption } from "../components/SelectDropdown/SelectDropdown";
 
-export const DEFAULT_VERSION = "v1.0.0";
-export const STABLE_VERSION = "v1.0.0";
+export const DEFAULT_VERSION = "v1.1.0";
+export const STABLE_VERSION = "v1.1.0";
 
 export const VERSION_SELECT_DEFAULT_OPTIONS: Array<SelectOption<string>> = [
   {
