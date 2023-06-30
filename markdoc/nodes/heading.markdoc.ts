e6159651b0a915f154456cff6c7ac6009b153f4b
Aniket Katkar <aniketkatkar97@gmail.com>
2023-06-30 16:50:38 +0530

    fixed the logic to create heading tag id from the content

--- markdoc/nodes/heading.markdoc.ts
+++ markdoc/nodes/heading.markdoc.ts
@@ -1,4 +1,26 @@
 import { Tag } from "@markdoc/markdoc";
+import { isEmpty, isString, toArray } from "lodash";
+
+// Logic to extract texts from the inline nodes
+// Example: # This is a `Heading`
+// should have id as "This-is-a-Heading"
+function getChildrenTexts(children): string[] {
+  if (isEmpty(children)) {
+    return [];
+  }
+
+  return toArray(children).reduce((prevValue, childNode) => {
+    if (isString(childNode)) {
+      return [...prevValue, childNode];
+    } else if (!isEmpty(childNode.attributes)) {
+      return [...prevValue, childNode.attributes.content];
+    } else if (!isEmpty(childNode.children)) {
+      return [...prevValue, ...getChildrenTexts(childNode.children)];
+    } else {
+      return;
+    }
+  }, []);
+}
 
 function generateID(children, attributes) {
   if (attributes.id && typeof attributes.id === "string") {
@@ -7,7 +29,8 @@ function generateID(children, attributes) {
   return children
     .filter((child) => typeof child === "string")
     .join(" ")
-    .replace(/[?]/g, "")
+    .replace(/[?:\-()%$#/@!;.,\/\\\[\]+=_]/g, "")
+    .trim()
     .replace(/\s+/g, "-")
     .toLowerCase();
 }
@@ -24,7 +47,8 @@ export const heading = {
   transform(node, config) {
     const attributes = node.transformAttributes(config);
     const children = node.transformChildren(config);
-    const id = generateID(children, attributes);
+
+    const id = generateID(getChildrenTexts(children), attributes);
 
     return new Tag(this.render, { ...attributes, id }, children);
   },
