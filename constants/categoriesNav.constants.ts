9d5997d573cb611595f57f6bab30faa04db06a85
David Goss <david@davidgoss.co>
2023-08-14 08:49:16 +0100

    fix: update paths for sdks
    SDK docs are now at e.g. https://docs.open-metadata.org/v1.1.0/sdk/java

--- constants/categoriesNav.constants.ts
+++ constants/categoriesNav.constants.ts
@@ -1,5 +1,5 @@
 export const API_AND_SDK_MENU_ITEMS = [
   { label: "REST API", value: "/swagger.html", withoutVersionPath: true },
-  { label: "Python", value: "/python-sdk", category: "SDK" },
-  { label: "Java", value: "/java-sdk", category: "SDK" },
+  { label: "Python", value: "/sdk/python", category: "SDK" },
+  { label: "Java", value: "/sdk/java", category: "SDK" },
 ];
