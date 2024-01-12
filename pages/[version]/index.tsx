46c9456f405ee40062194ad49854728c313c0a9a
Aniket Katkar <aniketkatkar97@gmail.com>
2024-01-12 17:29:49 +0530

    Minor: Add google tag manager changes for the pages (#216)
    * Add google tag manager changes for the pages
    
    * Update the script tag to use next js Script component for google tag manager
    
    * create a component for analytics script to use it commonly
    
    * Add analytics page to index.tsx in pages folder
    
    * update the component name

--- pages/[version]/index.tsx
+++ pages/[version]/index.tsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import CategoriesNav from "../../components/CategoriesNav/CategoriesNav";
 import ConnectorsInfo from "../../components/ConnectorsInfo/ConnectorsInfo";
 import Footer from "../../components/Footer/Footer";
+import GoogleAnalyticsScript from "../../components/GoogleAnalyticsScript/GoogleAnalyticsScript";
 import NewsEntry from "../../components/NewsEntry/NewsEntry";
 import { SelectOption } from "../../components/SelectDropdown/SelectDropdown";
 import TopNav from "../../components/TopNav/TopNav";
@@ -45,6 +46,7 @@ export default function Index({ versionsList }: Readonly<Props>) {
 
   return (
     <>
+      <GoogleAnalyticsScript />
       <div className="nav-bar-container">
         <TopNav versionsList={versionsList} />
         <CategoriesNav menu={menuItems} />
