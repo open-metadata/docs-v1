dea9bf5b4897b9c2d88bd156b1b8ae39a565e9a9
Aniket Katkar <aniketkatkar97@gmail.com>
2024-02-13 11:22:06 +0530

    center align the youtube videos

--- components/common/Youtube/Youtube.tsx
+++ components/common/Youtube/Youtube.tsx
@@ -1,59 +1,28 @@
+import classNames from "classnames";
+import { YouTubeProps } from "./Youtube.interface";
 import styles from "./Youtube.module.css";
 
-interface YouTubeProps {
-  videoId: string;
-  start?: string;
-  end?: string;
-  caption?: string;
-  height?: string;
-  width?: string;
-}
-
 const YouTube = ({
-  caption = "",
+  className = "",
   videoId,
   start = "",
   end = "",
   height,
   width,
 }: YouTubeProps) => {
-  let YouTubeBlock;
-
-  if (caption) {
-    YouTubeBlock = (
-      <section className={styles.Container}>
-        <section className={styles.IframeContainer}>
-          <iframe
-            allowFullScreen
-            src={`https://www.youtube-nocookie.com/embed/${videoId}?rel=0&start=${start}&end=${end}&rel=0`}
-            className={styles.Iframe}
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            style={{ height, width }}
-          ></iframe>
-        </section>
-        <section className={styles.CaptionContainer}>
-          <p className={styles.Caption}>{caption}</p>
-        </section>
-      </section>
-    );
-  } else {
-    YouTubeBlock = (
-      <section className={styles.IframeContainer} style={{ height, width }}>
-        <iframe
-          allowFullScreen
-          src={`https://www.youtube-nocookie.com/embed/${videoId}?rel=0&start=${start}&end=${end}&rel=0`}
-          className={styles.Iframe}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          style={{ height, width }}
-        ></iframe>
-      </section>
-    );
-  }
-  return YouTubeBlock;
+  return (
+    <div className={classNames(styles.Container, className)}>
+      <iframe
+        allowFullScreen
+        src={`https://www.youtube-nocookie.com/embed/${videoId}?rel=0&start=${start}&end=${end}&rel=0`}
+        className={styles.Iframe}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        style={{ height, width }}
+      ></iframe>
+    </div>
+  );
 };
 
 export default YouTube;
