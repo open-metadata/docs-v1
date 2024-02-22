c76ae027a3ca5bb5041808de2166843e0993d318
Aniket Katkar <aniketkatkar97@gmail.com>
2024-02-22 19:45:03 +0530

    update the build search index script

--- build_search_index.sh
+++ build_search_index.sh
@@ -3,7 +3,7 @@ partials="partials"
 for dir in content/*
   do 
     mydir="$(basename $dir)"
-    if [[ "$mydir" != "$partials" ]]
+    if [ "$mydir" != "$partials" ]
       then
         npx pagefind --site .next/server/pages/"$mydir" --output-path .next/static/chunks/pages/pageFind"$mydir";
     fi
