9aae283180c488fb99da83d3497bf18d38eacc90
Harsh Vador <harsh.vador@somaiya.edu>
2024-03-28 17:05:39 +0530

    adjust lcp

--- components/common/Youtube/Youtube.tsx
+++ components/common/Youtube/Youtube.tsx
@@ -1,3 +1,6 @@
+// YouTube.js
+
+import {useState, useEffect, useRef} from "react";
 import {YouTubeProps} from "./Youtube.interface";
 import styles from "./Youtube.module.css";
 import Head from "next/head";
@@ -5,14 +8,49 @@ import LiteYouTubeEmbed from "react-lite-youtube-embed";
 import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 
 const YouTube = ({videoId}: YouTubeProps) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.5,
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.unobserve(entry.target);
+        }
+      });
+    }, options);
+
+    if (videoRef.current) {
+      observer.observe(videoRef.current);
+    }
+
+    return () => {
+      if (videoRef.current) {
+        observer.unobserve(videoRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <div className="youtube-video-container">
+    <div
+      ref={videoRef}
+      className={`youtube-video-container ${isVisible ? "visible" : ""}`}
+    >
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <div className={`youtube-video ${styles.Iframe}`}>
-        <LiteYouTubeEmbed id={videoId} title="YouTube Video Player" />
-      </div>
+      {isVisible && (
+        <div className={`youtube-video ${styles.Iframe}`}>
+          <LiteYouTubeEmbed id={videoId} title="YouTube Video Player" />
+        </div>
+      )}
     </div>
   );
 };
