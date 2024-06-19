f5a34f86a0ecadb2994ca0c4f87ad99e4161f425
Aniket Katkar <aniketkatkar97@gmail.com>
2024-06-19 16:22:57 +0530

    Homepage content customization changes (#292)

--- components/common/HomePageBanner/HomePageBanner.tsx
+++ components/common/HomePageBanner/HomePageBanner.tsx
@@ -1,19 +1,19 @@
 import classNames from "classnames";
 import Link from "next/link";
-import {useCallback} from "react";
-import {
-  BANNER_LINKS_INFO,
-  HOME_PAGE_BANNER_INFO,
-} from "../../../constants/homePage.constants";
-import {useDocVersionContext} from "../../../context/DocVersionContext";
-import {ReactComponent as ArrowRight} from "../../../images/icons/arrow-right.svg";
-import {getUrlWithVersion} from "../../../utils/CommonUtils";
+import { Fragment, useCallback } from "react";
+import { useDocVersionContext } from "../../../context/DocVersionContext";
+import { ReactComponent as ArrowRight } from "../../../images/icons/arrow-right.svg";
+import { getUrlWithVersion } from "../../../utils/CommonUtils";
 import Button from "../Button/Button";
 import YouTube from "../Youtube/Youtube";
+import { HomePageBannerProps } from "./HomePageBanner.interface";
 import styles from "./HomePageBanner.module.css";
 
-export default function HomePageBanner() {
-  const {docVersion} = useDocVersionContext();
+export default function HomePageBanner({
+  quickLinks,
+  bannerInfo,
+}: Readonly<HomePageBannerProps>) {
+  const { docVersion } = useDocVersionContext();
 
   const getLinkButtonStyleFromTheme = useCallback(
     (theme: string) => {
@@ -35,50 +35,50 @@ export default function HomePageBanner() {
     <div className={styles.Container}>
       <div className={styles.Content}>
         <div className="mb-8 flex flex-col items-center">
-          <div className={styles.Heading}>{HOME_PAGE_BANNER_INFO.title}</div>
+          <div className={styles.Heading}>{bannerInfo.title}</div>
           <section className={styles.Divider} />
-          <p className="text-xl text-center">
-            {HOME_PAGE_BANNER_INFO.description}
-          </p>
+          <p className="text-xl text-center">{bannerInfo.description}</p>
         </div>
       </div>
       <div className={styles.Video}>
         <YouTube videoId="cVYP1HFXeRM" />
       </div>
       <div className={styles.BannerNavLinkContainer}>
-        {BANNER_LINKS_INFO.map(
+        {quickLinks.map(
           (
-            {title, description, linkTitle, href, theme, externalURL},
+            { title, description, linkTitle, href, theme, externalURL },
             index
           ) => (
-            <div
-              className={classNames(styles.BannerNavLink, {
-                [styles.BannerNavLinkBorder]: index !== 0,
-              })}
-              key={href}
-            >
-              <div>
-                <div className={styles.SubHeading}>{title}</div>
-                <p className={styles.DescriptionText}>{description}</p>
-              </div>
-              <Link
-                href={externalURL ? href : getUrlWithVersion(href, docVersion)}
-                target={externalURL ? "_blank" : "_self"}
-              >
-                <Button
-                  className={classNames(
-                    styles.BannerNavButton,
-                    getLinkButtonStyleFromTheme(theme)
-                  )}
-                  type="button"
+            <Fragment key={href}>
+              <div className={styles.BannerNavLink}>
+                <div>
+                  <div className={styles.SubHeading}>{title}</div>
+                  <p className={styles.DescriptionText}>{description}</p>
+                </div>
+                <Link
+                  href={
+                    externalURL ? href : getUrlWithVersion(href, docVersion)
+                  }
+                  target={externalURL ? "_blank" : "_self"}
                 >
-                  <span className="font-semibold">{linkTitle}</span>
-                  <span className="ml-2">
-                    <ArrowRight />
-                  </span>
-                </Button>
-              </Link>
-            </div>
+                  <Button
+                    className={classNames(
+                      styles.BannerNavButton,
+                      getLinkButtonStyleFromTheme(theme)
+                    )}
+                    type="button"
+                  >
+                    <span className="font-semibold">{linkTitle}</span>
+                    <span className="ml-2">
+                      <ArrowRight />
+                    </span>
+                  </Button>
+                </Link>
+              </div>
+              {index < quickLinks.length - 1 && (
+                <div className={styles.BannerNavLinkBorder} />
+              )}
+            </Fragment>
           )
         )}
       </div>
