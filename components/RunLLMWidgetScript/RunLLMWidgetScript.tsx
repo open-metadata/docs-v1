3cf92e583301dd25c87c73678561dc37ad484d13
Sriharsha Chintalapani <harshach@users.noreply.github.com>
2024-08-06 22:05:28 -0700

    Update RunLLMWidgetScript.tsx slack url (#309)

--- components/RunLLMWidgetScript/RunLLMWidgetScript.tsx
+++ components/RunLLMWidgetScript/RunLLMWidgetScript.tsx
@@ -12,7 +12,7 @@ export function RunLLMWidgetScript() {
       runllm-name="OpenMetadata"
       runllm-brand-logo="https://avatars.githubusercontent.com/u/86132257?s=200&v=4"
       runllm-community-type="slack"
-      runllm-community-url="https://openmetadata.slack.com/join/shared_invite/zt-2muq1e0tw-igZZQLgnn9etFmWACHUdMg"
+      runllm-community-url="https://slack.open-metadata.org"
     ></script>
   );
 }
