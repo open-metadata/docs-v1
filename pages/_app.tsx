622e87b8cf27e7d349e6f90d84a15d081076fdc1
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2024-09-16 21:49:22 +0530

    Remove TrackingPixelScript component and update _app.tsx (#323)
    * Remove TrackingPixelScript component and update _app.tsx
    
    * Remove TrackingPixelScript component and update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,25 @@
-import Head from "next/head";
+import Head from 'next/head';
 
-import "prismjs";
+import 'prismjs';
 // Import other Prism themes here
-import "../public/globals.css";
-import "../public/modal.css";
+import '../public/globals.css';
+import '../public/modal.css';
 
-import { isEmpty } from "lodash";
-import type { AppProps } from "next/app";
-import ErrorBoundary from "../components/ErrorBoundary";
-import GoogleAnalyticsScript from "../components/GoogleAnalyticsScript/GoogleAnalyticsScript";
-import TrackingPixelScript from "../components/TrackingPixelScript/TrackingPixelScript";
-import { CodeWithLanguageSelectorContextProvider } from "../context/CodeWithLanguageSelectorContext";
-import { DocVersionContextProvider } from "../context/DocVersionContext";
-import { MenuItemsContextProvider } from "../context/MenuItemsContext";
-import { NavBarCollapseContextProvider } from "../context/NavBarCollapseContext";
-import { RouteChangingContextProvider } from "../context/RouteChangingContext";
-import { StepsContextProvider } from "../context/StepsContext";
-import { SlugProps } from "./[version]/[...slug]";
+import { isEmpty } from 'lodash';
+import type { AppProps } from 'next/app';
+import ErrorBoundary from '../components/ErrorBoundary';
+import GoogleAnalyticsScript from '../components/GoogleAnalyticsScript/GoogleAnalyticsScript';
+import { CodeWithLanguageSelectorContextProvider } from '../context/CodeWithLanguageSelectorContext';
+import { DocVersionContextProvider } from '../context/DocVersionContext';
+import { MenuItemsContextProvider } from '../context/MenuItemsContext';
+import { NavBarCollapseContextProvider } from '../context/NavBarCollapseContext';
+import { RouteChangingContextProvider } from '../context/RouteChangingContext';
+import { StepsContextProvider } from '../context/StepsContext';
+import { SlugProps } from './[version]/[...slug]';
 
-const TITLE = "OpenMetadata Documentation: Get Help Instantly";
+const TITLE = 'OpenMetadata Documentation: Get Help Instantly';
 const DESCRIPTION =
-  "Follow the step-by-step guides to get started with OpenMetadata, the #1 open source data catalog tool. Get discovery, collaboration, governance, observability, quality tools all in one place.";
+  'Follow the step-by-step guides to get started with OpenMetadata, the #1 open source data catalog tool. Get discovery, collaboration, governance, observability, quality tools all in one place.';
 
 export default function MyApp({ Component, pageProps }: AppProps<SlugProps>) {
   const title = isEmpty(pageProps.pageTitle) ? TITLE : pageProps.pageTitle;
@@ -49,7 +48,6 @@ export default function MyApp({ Component, pageProps }: AppProps<SlugProps>) {
         <meta content="summary_large_image" name="twitter:card" />
       </Head>
       <GoogleAnalyticsScript />
-      <TrackingPixelScript />
       <ErrorBoundary>
         <RouteChangingContextProvider>
           <DocVersionContextProvider>
