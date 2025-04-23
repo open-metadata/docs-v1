e3eda900641c7011e705b97c9be6ad7c3db4d012
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2025-04-23 16:18:28 +0530

    update tag script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -47,17 +47,17 @@ export default function MyApp({ Component, pageProps }: AppProps<SlugProps>) {
         />
         <meta property="og:type" content="website" />
         <meta content="summary_large_image" name="twitter:card" />
-        <GoogleAnalyticsScript />
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-554C968W');`,
-          }}
-        />
       </Head>
+      <GoogleAnalyticsScript />
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-554C968W');`,
+        }}
+      />
       <ErrorBoundary>
         <RouteChangingContextProvider>
           <DocVersionContextProvider>
