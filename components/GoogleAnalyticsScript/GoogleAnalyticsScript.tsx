6e161f2b51f96fcc49bd02e2b57a95743bd12047
Sweta Agarwalla <105535990+sweta1308@users.noreply.github.com>
2025-04-24 14:55:23 +0530

    update gtm to remove ga script (#379)

--- components/GoogleAnalyticsScript/GoogleAnalyticsScript.tsx
+++ components/GoogleAnalyticsScript/GoogleAnalyticsScript.tsx
@@ -2,18 +2,6 @@ import Script from "next/script";
 
 function GoogleAnalyticsScript() {
   return (
-    <>
-      {/* Google tag (gtag.js) */}
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-2TW1XM6C89" />
-      <Script id="google-analytics">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
- 
-          gtag('config', 'G-2TW1XM6C89');
-        `}
-      </Script>
       <Script
         dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -23,7 +11,6 @@ function GoogleAnalyticsScript() {
             })(window,document,'script','dataLayer','GTM-554C968W');`,
         }}
       />
-    </>
   );
 }
 
