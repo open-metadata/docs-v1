c559129d492bf240b061002b8cca1aee976570d0
Aniket Katkar <aniketkatkar97@gmail.com>
2025-08-13 15:11:31 +0530

    gitingnore .docusaurus

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/aniket/Desktop/docs-v1/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/aniket/Desktop/docs-v1/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/aniket/Desktop/docs-v1/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/aniket/Desktop/docs-v1/src/css/custom.css"),
-];
